d4a51d6b694fd0f677afc3646de85f57dc95808e
Md Nuruddin <nuruddinalways99@gmail.com>
2023-02-23 22:37:00 +0600

    Cetegories components

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,50 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Categories = () => {
+  
+  const { products } = useSelector((state) => state.products);
+
   return (
     <>
       <div className='md:mx-14 my-16'>
         <div className='md:grid md:grid-cols-8  grid grid-cols-2 md:space-y-0 items-center'>
-          <CateCard
-            img='/images/product-1.jpg'
-            title='Fashion'
-            caption='7 Products'
-          />
-          <CateCard
-            img='/images/product-2.jpg'
-            title='Furniture'
-            caption='12 Products'
-          />
-          <CateCard
-            img='/images/product-3.jpg'
-            title='Headphones'
-            caption='4 Products'
-          />
-          <CateCard
-            img='/images/product-4.jpg'
-            title='Caps'
-            caption='5 Products'
-          />
-          <CateCard
-            img='/images/product-5.jpg'
-            title='Gamings'
-            caption='8 Products'
-          />
-          <CateCard
-            img='/images/product-6.jpg'
-            title='Drones'
-            caption='10 Products'
-          />
-          <CateCard
-            img='/images/product-14.jpg'
-            title='Watch'
-            caption='20 Products'
-          />
-          <CateCard
-            img='/images/product-10.jpg'
-            title='Cameras'
-            caption='15 Products'
-          />
+          {products.slice(0, 8).map((product) => (
+            <CateCard
+              img={product.image}
+              title={product.title}
+              caption='15 Products'
+            />
+          ))}
         </div>
       </div>
     </>
@@ -60,7 +31,7 @@ const CateCard = ({ img, title, caption }) => {
         alt='product-1'
       />
       <div className='space-y-1'>
-        <h1 className='text-xl font-semibold'>{title}</h1>
+        <h1 className='text-xl font-semibold'>{title.slice(0, 10)}</h1>
         <p className='text-xs font-medium uppercase text-[#3d3d3d]'>
           {caption}
         </p>
